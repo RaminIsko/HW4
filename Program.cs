eacade87dcc5d2905d5c4bf6e43ac2e479c1ec27
RaminIsko <ramsmagast@gmail.com>
2023-02-19 20:14:42 +0600

    first exercise

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ЗАДАЧА, выводащая a в степени b");
+int term (int num1,int num2){
+    int result = Convert.ToInt32(Math.Pow(num1, num2));
+    return result;
+}
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Первое число в степени второго числа: {term(num1, num2)}");
